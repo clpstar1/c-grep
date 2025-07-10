dcd719ffe54a222ef9f30c1d6411a28147d9b382
steffen.lesch96@proton.me <steffen.lesch96@proton.me>
2025-07-10 16:03:25 +0200

    fix wrong match condition

--- grep.c
+++ grep.c
@@ -8,130 +8,84 @@
 #define TRUE 1
 #define FALSE 0
 
-int match_line(char * line, char * pattern) {
-  int l_idx = 0;
-  int l_len = strlen(line);
-  int p_idx = 0;
-  int p_len = strlen(pattern);
+int match(char * s, char * p) {
+  // empty p does not match any string
+  if (strlen(s) == 0 && strlen(p) == 0) return TRUE;
+  if (strlen(s) > 0 && strlen(p) == 0) return FALSE;
 
-  // pattern is not consumed yet
-  while (p_idx < p_len) {
-    
-    if (l_idx >= l_len) {
-      return FALSE;
-    }
-
-    char p_cur = pattern[p_idx];
-    char l_cur = line[l_idx];
-    // printf("%c\n", p_cur);
-
-    if (p_cur == '[') {
-      int match = FALSE; 
-      // consume characters until a character in the group is met or string is empty
-      while (l_idx++ < l_len) {
-        l_cur = line[l_idx];
-        int group_start = p_idx + 1;
-        while ((p_cur = pattern[group_start++]) != ']') {
-          if (group_start > p_len) {
-            printf("ERROR: unmatched bracket");
-            exit(1);
-          }
-          if (p_cur == l_cur) { 
-            match = TRUE;
-            break;
-          } 
-        }
+  while (*p != '\0' && *s != '\0') {
+    // character group
+    if (*p == '[') {
+      // TODO Copy the inner pattern and match it
+    } 
+    // character class
+    else if (*p == '\\') {
+      if (*(p++) == '\0') {
+        printf("ERROR: unclosed character class");
+        exit(1);
       }
-      return match;
-    }
-    else if (p_cur == '\\') {
-      // consume characters until class is met or string is empty
-      int match = FALSE;
-      p_idx++;
-      char class = pattern[p_idx];
-      while (l_idx < l_len) {
-        l_cur = line[l_idx];
-        switch (class) {
-          case 'd':
-              match = isdigit(l_cur);
-              break;
-          case 'w':
-              match = isalpha(l_cur);
-              break;
-          default:
-            printf("ERROR: unrecognized character class");
-            exit(EXIT_FAILURE);
-        }
-        if (match) {
-            return TRUE;
-        }
-        l_idx++;
-      }
-      return match;
-    }
-    // consume characters until p_cur is met or line is empty
-    else {
-      // matched, increase both
-      if (p_cur == l_cur) {
-        l_idx++;
-        p_idx++;
-      }
-      // mismatch 
-      else {
-        l_idx++;
-      }
-    }
-    
-  }
-
-  // pattern consumed, line still has characters left 
-  if (l_idx < l_len - 1) {
-    return FALSE;
-  }
-
-  return TRUE; 
-
-  for (int i = 0; line[i] != '\0'; i++) {
-
-    if (pattern[0] == '[') {
-      // try each letter in the character group for each letter in line
-    }
-
-    if (pattern[0] == '\\') {
-      switch (pattern[1]) {
+      switch (*p) {
         case 'd':
-          if (isdigit(line[i])) {
-            return 0;
+          while (*s != '\0' && !isdigit(*s)) {
+            s++;
           }
           break;
         case 'w':
-          if (isalpha(line[i])) {
-            return 0;
+          while (*s != '\0' && !isalpha(*s)) {
+            s++;
           }
           break;
+        default:
+          printf("ERROR: unrecognized character class");
+          exit(1);
       }
+      // no char class in string 
+      if (*s == '\0') {
+        return FALSE;
+      }
+      p++; s++;
+    } 
+    // exact match
+    else if (*p == *s) {
+      p++; s++;
+    // no match
+    } else {
+      s++;
     }
-
-    if (line[i] == pattern[0]) {
-      return 0;
-    }
-    
   }
-  return 1;
+
+  return *p == '\0';
 }
 
 int main(int argc, char * argv[]) {
-  assert(match_line("abc", "bc") == TRUE);
-  assert(match_line("abc", "bbc") == FALSE);
-  assert(match_line("Hello", "o") == TRUE);
-  assert(match_line("Hello", "x") == FALSE);
-  assert(match_line("Hell1", "\\d") == TRUE);
-  assert(match_line("Hello", "\\d") == FALSE);
-  assert(match_line("Hello", "\\w") == TRUE);
-  assert(match_line("!?$", "\\w") == FALSE);
+  assert(match("", "") == TRUE);
 
-  assert(match_line("Hello", "[abe]") == TRUE);
-  assert(match_line("Hello", "[abc]") == FALSE);
+  // simple matches
+  //
+  // |a|0   -> a|0|
+  // |a|0   -> a|0|
+  assert(match("a", "a") == TRUE);
+
+  // |a|b0  -> a|b|0
+  // |b|0   -> |b|0
+  assert(match("ab", "b") == TRUE);
+
+  // |a|b0  -> |b|0
+  // |a|0   -> |0|
+  assert(match("ab", "a") == TRUE);
+  //
+  // |a|0   -> a|0|
+  // |b|0   -> a|0|
+  assert(match("b", "a") == FALSE);
+
+}
+  // assert(match("Hell1", "\\d") == TRUE);
+  // assert(match("a", "\\d") == FALSE);
+  // assert(match("wa", "\\w\\w") == TRUE);
+  // assert(match("!?$", "\\w") == FALSE);
+  //
+  // assert(match_line("Hello", "[abe]") == TRUE);
+  // assert(match_line("Hello", "[abc]") == FALSE);
 
   // if (argc != 2) {
   //   printf("USAGE: crepe PATTERN\n");
@@ -151,4 +105,4 @@ int main(int argc, char * argv[]) {
   //
   // return 0;
   
-}
+
