f34593abe26347b4ad0907ea3cc5543f2a015467
steffen.lesch96@proton.me <steffen.lesch96@proton.me>
2025-07-29 14:56:16 +0200

    wip capture group

--- cgrep.c
+++ cgrep.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #define BUFSZ 1024
 #define ASSERT(cond)                                                           \
@@ -12,312 +13,286 @@
     }                                                                          \
   } while (0)
 
-bool match(char *s, char *p);
+bool match_start;
+bool match_end;
+bool did_match;
 
-typedef enum quantifier { Single, Plus, Star } quantifier;
-
-typedef struct token {
-  char * val; 
-  quantifier quant;
-} token;
-
-typedef struct token_array {
-  token * t;
+struct pattern {
+  struct token **tokens;
   int length;
-} token_array;
+  struct pattern *alternative;
+};
 
-char * mk_character_class(char class) {
-  char * c = malloc(sizeof(char) * 3);
-  c[0] = '\\';
-  c[1] = class;
-  c[2] = '\0';
-  return c;
+typedef enum quant_t {
+  NONE,
+  STAR,
+  PLUS
+} quant_t;
+
+typedef enum token_t {
+  CHAR,
+  ESCAPE,
+  CHARACTER_CLASS,
+  CAPTURE_GROUP,
+} token_t;
+
+struct token {
+  quant_t quantifier;
+  token_t type; 
+  union {
+    char ch;
+    char *cclass;
+    char *inner;
+  } v;
+};
+
+void abort_with_message(char *message) {
+  printf("%s\n", message);
+  exit(1);
 }
 
-char * mk_character(char val) {
-  char * c = malloc(sizeof(char) * 2);
-  c[0] = val;
-  c[1] = '\0';
-  return c;
+char *extract_inner(char *start, char *end){
+  if (start >= end) return NULL;
+  int ssize = end - start;
+  char *inner_s = malloc(sizeof(char) * ssize);
+  strncpy(inner_s, start+1, ssize);
+  inner_s[ssize-1] = '\0';
+  return inner_s;
 }
 
-char * mk_character_group(char * pattern) {
-  int end = 0;
+struct pattern *mk_token_arr(char *p) {
+  struct pattern *arr = malloc(sizeof(struct pattern));
+  if (*p == '\0') return NULL;
+  arr->tokens = malloc(sizeof(struct token) * strlen(p));
+  arr->length = 0;
 
-  char pcur;
-  do {
-    pcur = pattern[end];
-    if (pcur == ']' && end < 2) {
-      printf("ERROR: empty character group");
-      exit(1);
-    }
-    end++;
-  } while (pcur != ']');
-
-  char * ptr = malloc(sizeof(char) * (end + 1));
-  
-  for (int i = 0; i <= end; i++) {
-    ptr[i] = pattern[i];
-    if (i == end) {
-      ptr[i] = '\0';
-    }
-  }
-
-  return ptr; 
-}
-
-char * mk_alternation(char * pattern) {
-  int token_idx = 1; // skip opening paren 
-  int option_sz = 0;
-  char pcur; 
-  while ((pcur = pattern[token_idx]) != ')') {
-    if (pcur == '\0') {
-      printf("ERROR: unclosed alternation");
-      exit(1);
-    }
-    if (pcur == '|') {
-      if (option_sz == 0) {
-        printf("ERROR: empty alternation option");
-        exit(1);
-      }
-      option_sz = 0;
-    }
-    option_sz++;
-    token_idx++;
-  }
-  if (token_idx == 1) {
-    printf("ERROR: empty alternation");
-    exit(1);
-  }
-  char * val = malloc(sizeof(char) * (token_idx+1));
-  for (int i = 0; i <= token_idx; i++) {
-    if (i == token_idx) {
-      val[i] = '\0';
-    }
-    val[i] = pattern[i];
-  }
-  return val;
-}
-
-token_array * tokenize_pattern(char * p) {
-  token * t = malloc(sizeof(token) * (strlen(p)));
-  int ti = 0; 
-  int pi = 0;
-
-  while (p[pi] != '\0') {
-    if (pi == 0 && p[pi] == '^') {
-      pi++;
+  while (*p != '\0') {
+    if (*p == '$' && *(p+1) == '\0') {
+      p++;
       continue;
     }
-    
-    if (pi == strlen(p) - 1 && p[pi] == '$') {
-      pi++;
-      continue;
+    struct token *t = malloc(sizeof(struct token));
+    t->quantifier = NONE;
+    if (*p == '\\') {
+      t->type = ESCAPE;
+      if (*(p+1) == '\0') abort_with_message("ERROR: empty escape sequence");
+      p++;
+      t->v.ch = *p;
     }
+    else if (*p == '(') {
+      char *end = p;
+      while ((end = strrchr(end, ')')) != NULL && *(end-1) == '\\');
+      if (end == NULL) abort_with_message("ERROR: unclosed capture group");
+      char *inner = extract_inner(p, end);
+      if (inner == NULL) abort_with_message("ERROR: empty capture group");
+      t->type = CAPTURE_GROUP;
+      t->v.inner = inner;
+      p = end;
+    }
+    else if (*p == '|') {
+      if (*(p+1) == '\0') abort_with_message("ERROR: empty alternative");
+      arr->alternative = mk_token_arr(p+1);
+      return arr;
+    }
+    else if (*p == ']') {
+      abort_with_message("ERROR: unclosed character class");
+    }
+    else if (*p == '[') {
+      // copy the inner characters of [] to cclass;
+      char *end = p;
+      while ((end = strchr(end, ']')) != NULL && *(end-1) == '\\');
+      if (end == NULL) abort_with_message("ERROR: unclosed character class");
 
-    if (p[pi] == '*') {
-      if (ti > 0) {
-        t[ti-1].quant = Star;
-      }
-      pi++;
+      char *inner = extract_inner(p, end);
+      if (inner == NULL) abort_with_message("ERROR: empty character class");
+      t->type = CHARACTER_CLASS;
+      t->v.cclass = inner;
+      p = end;
     }
-    else if (p[pi] == '+') {
-      if (ti > 0) {
-        t[ti-1].quant = Plus;
-      }
-      pi++;
-    }
-
     else {
-      token * tcur = &t[ti];
-      tcur->quant = Single;
-
-      if (p[pi] == '(') {
-        tcur->val = mk_alternation(&p[pi]);
-        while(p[++pi] != ')');
-      }
-      else if (p[pi] == '[') {
-        tcur->val = mk_character_group(p);
-        while (p[++pi] != ']');
-      }
-      else if (p[pi] == '\\') {
-        pi++;
-        switch (p[pi]) {
-          case 'd':
-            tcur->val = mk_character_class('d');
-            break;
-          case 'w':
-            tcur->val = mk_character_class('w');
-            break;
-          default:
-            tcur->val = mk_character_class(p[pi]);
-        }
-      }
-      else {
-        tcur->val = mk_character(p[pi]);
-      }
-      ti++;
-      pi++;
+      t->type = CHAR;
+      t->v.ch = *p;
     }
+    p++;
+    if (*p == '*') {
+      t->quantifier = STAR;
+      p++;
+    }
+    if (*p == '+') {
+      t->quantifier = PLUS;
+      p++;
+    }
+    arr->tokens[arr->length] = t;
+    arr->length++;
   }
-  token_array * arr = malloc(sizeof(token_array));
-  arr->t = t;
-  arr->length = ti;
   return arr;
 }
 
-bool match(char *s, char *p);
 
-bool match_alternation_option(char *s, char *opt_start, size_t opt_size) {
-    char *alternation = malloc(sizeof(char) * opt_size + 1);
-    memcpy(alternation, opt_start, opt_size);
-    alternation[opt_size] = '\0';
-    return match(s, alternation);
+void print_token(struct token *t) {
+    printf("type = %d, quantifier = %d, ", t->type, t->quantifier);
+    if (t->type == CHAR || t->type == ESCAPE) {
+      printf("value = %c\n", t->v.ch);
+    }
+    else if (t->type == CHARACTER_CLASS) {
+      printf("value = %s\n", t->v.cclass);
+    }
+    else if (t->type == CAPTURE_GROUP) {
+      printf("value = %s\n", t->v.inner);
+    }
 }
 
-bool match_token(char * s, char * t) {
-  if (*t == '.') return true;
-  if (*t == '\\') {
-    t++;
-    switch (*t) {
-      case 'd': return isdigit(*s);
-      case 'w': return isalpha(*s);
-      default: return *s == *t;
-    }
+void _print_token_arr(struct pattern *arr, char *s) {
+  for (int i = 0; i < arr->length; i++) {
+    struct token *t = arr->tokens[i];
+    print_token(t);
   }
-  if (*t == '(') {
-    t++;
-    char *opt_end = t;
-    int opt_size = 0; 
-    while (*opt_end != ')') {
-      if (*opt_end == '|') {
-        if (match_alternation_option(
-          s,
-          t,
-          opt_size)) {
-          return true;
+}
+
+void print_token_arr(struct pattern *arr, char *s) {
+  while (arr != NULL) {
+    _print_token_arr(arr, s);
+    arr = arr->alternative;
+  }
+}
+
+bool __match(char *s, struct pattern *arr);
+
+bool match(char *s, char *p);
+
+bool _match_token(char *s, struct token *t) {
+  print_token(t);
+  if (t->type == CAPTURE_GROUP) {
+    return match(s, t->v.inner);
+  }
+  if (t->type == CHAR) {
+    if (t->v.ch == '.') return true;
+    return *s == t->v.ch;
+  }
+  else if (t->type == ESCAPE) {
+    if (t->v.ch == 'd') return isdigit(*s);
+    if (t->v.ch == 'w') return isalpha(*s);
+    else return *s == t->v.ch;
+  }
+  else if (t->type == CHARACTER_CLASS) {
+    bool negate = t->v.cclass[0] == '^';
+    for (int i = 0; t->v.cclass[i] != '\0'; i++) {
+      if (t->v.cclass[i] == *s) return !negate;
+    }
+    return negate; 
+  }
+  abort_with_message("ERROR: unknown token type");
+  return false;
+}
+
+bool match_token(char *s, struct token *t) {
+  bool is_match = _match_token(s, t);
+  if (!did_match) {
+    did_match = is_match;
+  }
+  return is_match;
+}
+
+bool __match(char *s, struct pattern *arr) {
+  int pi = 0; 
+  while (*s != '\0' && pi < arr->length) {
+    struct token *t = arr->tokens[pi];
+    struct token *next = NULL;
+
+    if (pi < arr->length - 1) {
+      next = arr->tokens[pi+1];
+    }
+
+    switch (t->quantifier) {
+      // match 1 to n
+      case PLUS:
+        if (!match_token(s, t)) {
+          if (did_match || match_start) return false;
+          // try again with the next char in s
+          s++;
+          continue;
         }
-        opt_size = 0;
-        opt_end++;
-        t = opt_end;
-      }
-      else {
-        opt_size++;
-        opt_end++;
-      }
-    }
-    return match_alternation_option(s, t, opt_size);
-  }
-  if (*t == '[') {
-    t++;
-    bool is_negative_group = false;
-    if (*t == '^') {
-      is_negative_group = true;
-      t++;
-    }
-    char * s_iter;
-    while (*t != ']') {
-      s_iter = s;
-      while (*s_iter != '\0') {
-        if (match_token(s_iter, t)) {
-          return !is_negative_group;
+        s++;
+      // match 0 to n
+      case STAR:
+        while(*s != '\0' && match_token(s, t)) {
+          if (next != NULL && match_token(s, next)) {
+            break;
+          }
+          s++;
         }
-        s_iter++;
-      }
-      t++;
+        pi++;
+        break;
+      default:
+        if (!match_token(s, t)) {
+          if (did_match || match_start) return false;
+          // try again with the next char in s
+          s++;
+          continue;
+        } 
+        pi++;
+        s++;
     }
-    return is_negative_group;
   }
-  return *s == *t;
+  if (match_end) return *s == '\0' && pi == arr->length;
+  return pi == arr->length;
 }
 
 bool match(char *s, char *p) {
-  if (strlen(s) == 0 && strlen(p) == 0) return true;
-  if (strlen(s) > 0 && strlen(p) == 0) return false;
+  if (s == NULL || p == NULL) {
+    abort_with_message("ERROR: string or pattern to match must not be null");
+  }
+  if (*s == '\0') return *p == '\0' || *(p+1) == '*';
+  if (*p == '\0') return *s == '\0';
+  match_start = false;
+  match_end = false;
 
-  bool has_start_anchor = *p == '^';
-  bool has_end_anchor = *(p + strlen(p) - 1) == '$';
+  did_match = false;
+  if (*p == '^') {
+    match_start = true;
+    p++;
+  }
+  if (*(p+strlen(p) - 1) == '$') {
+    match_end = true;
+  }
+  struct pattern *t = mk_token_arr(p);
+  // print_token_arr(t, s);
   
-  token_array arr = *tokenize_pattern(p);
-  token * tokens = arr.t; 
-  int ti = 0;
+  while(t != NULL) {
+    if(__match(s, t)) return true;
+    t = t->alternative;
+  }
+  return false;
+}
 
-  // track if a match has occured, if $ is present at the end of the string 
-  // everything has to match from here on out
-  bool did_match = false;
+void test_char_only() {
+  ASSERT(match("", "") == true);
+  // ASSERT(match("a", "") == true);
 
-  // for (int i = 0; i < arr.length; i++) {
-  //   printf("val = %s, quant = %d\n", tokens[i].val, tokens[i].quant);
-  // }
+  ASSERT(match("ab", "b") == true);
+  ASSERT(match("ab", "a") == true);
+  ASSERT(match("b", "a") == false);
+  ASSERT(match("aba", "aa") == false);
+  ASSERT(match("bab", "a") == true);
+  ASSERT(match("a\n", "a") == true);
+}
 
-  do {
-    token t = tokens[ti];
-    char * val = t.val;
-    bool match = false;
+void test_character_class() {
+  ASSERT(match("a1", "\\d") == true);
+  ASSERT(match("1a", "\\d") == true);
+  ASSERT(match("a1a", "\\d") == true);
+  ASSERT(match("a", "\\d") == false);
+  ASSERT(match("\\d", "\\\\d") == true);
+  ASSERT(match("1 apple", "\\d apple") == true);
+  ASSERT(match("1 orange", "\\d apple") == false);
+  ASSERT(match("100 apples", "\\d\\d\\d apple") == true);
+  ASSERT(match("1 apple", "\\d\\d\\d apple") == false);
+  ASSERT(match("3 dogs", "\\d \\w\\w\\ws") == true);
+  ASSERT(match("4 cats", "\\d \\w\\w\\ws") == true);
+  ASSERT(match("1 dog", "\\d \\w\\w\\ws") == false);
 
-    char * next = NULL; 
-    if (ti+1 < arr.length) {
-      next = tokens[ti+1].val;
-    }
-
-    switch (t.quant) {
-      // match between 1 and n times 
-      case Plus:
-        match = match_token(s, val);
-        // no match means advance the string but NOT advancing the pattern
-        // - in s = ba and p = a+, 'a+' only matches the second a and b is ignored
-        if (!match) {
-          s++;
-          break;
-        }
-        // consume until token no longer matches or s is empty
-        s++;
-        while (*s != '\0' && match_token(s, val)) {
-          // if the next token in the pattern also matches our greedy consumption must end
-          // otherwise in the pattern "d.+g" and the string dog, '.+' consumes everything since '.+' also matches 'g'
-          // this would result in the pattern not being fully consumed when the string is fully consumed
-          if (next != NULL && match_token(s, next)) break; 
-          s++;
-        }
-        ti++;
-        break;
-      // match between 0 and n times
-      case Star:
-        // always true since 0 matches are allowed
-        match = true;
-        // consume until token no longer matches or s is empty
-        while (*s != '\0' && match_token(s, val)) {
-          // see explanation for Plus quantifier
-          if (next != NULL && match_token(s, next)) break; 
-          s++;
-        }
-        // here we can always advance the pattern as every char of s is a positive match
-        ti++;
-        break;
-      default:
-        match = match_token(s, val);
-        if (match) {
-          ti++;
-        }
-        s++;
-        break;
-    }
-
-    if (match) {
-      did_match = true;
-    }
-
-    if (!match) {
-      if (did_match) return false;
-      if (has_start_anchor) return false;
-    }
-
-  } while (ti < arr.length && *s != '\0');
-
-  
-  return has_end_anchor 
-    ? ti == arr.length && *s == '\0'
-    : ti == arr.length;
+  ASSERT(match(".", "\\.") == true);
+  ASSERT(match("\\", "\\") == true);
 }
 
 void test_wildcard() {
@@ -331,89 +306,69 @@ void test_wildcard() {
   ASSERT(match("ad..g", "^d\\.+g$") == false);
 }
 
-void test_char_only() {
-  ASSERT(match("", "") == true);
-  ASSERT(match("a", "") == false);
-  //
-  ASSERT(match("ab", "b") == true);
-  ASSERT(match("ab", "a") == true);
-  ASSERT(match("bab", "a") == true);
-  ASSERT(match("b", "a") == false);
-  ASSERT(match("aba", "aa") == false);
-
-  ASSERT(match("a\n", "a") == true);
-}
-
-void test_character_class() {
-  ASSERT(match("a1", "\\d") == true);
-  ASSERT(match("1a", "\\d") == true);
-  ASSERT(match("a1a", "\\d") == true);
-  ASSERT(match("a", "\\d") == false);
-  ASSERT(match("\\d", "\\\\d") == true);
-  ASSERT(match("a", "\\d") == false);
-
-  ASSERT(match("1 apple", "\\d apple") == true);
-  ASSERT(match("1 orange", "\\d apple") == false);
-  ASSERT(match("100 apples", "\\d\\d\\d apple") == true);
-  ASSERT(match("1 apple", "\\d\\d\\d apple") == false);
-  ASSERT(match("3 dogs", "\\d \\w\\w\\ws") == true);
-  ASSERT(match("4 cats", "\\d \\w\\w\\ws") == true);
-  ASSERT(match("1 dog", "\\d \\w\\w\\ws") == false);
-
-  ASSERT(match(".", "\\.") == true);
-}
-
 void test_groups() {
   ASSERT(match("abc", "[a]") == true);
-  ASSERT(match("abc", "[e]") == false);
+  ASSERT(match("a", "[e]") == false);
 
   ASSERT(match("abc", "[^abc]") == false);
   ASSERT(match("def", "[^abc]") == true);
-
-  ASSERT(match("abc", "[[a]]") == true);
 }
 
 void test_anchors() {
-  ASSERT(match("slogs", "^slog") == true);
-  ASSERT(match("slogsa", "^slog") == true);
-  ASSERT(match("slogs", "^log") == false);
+  ASSERT(match("", "^") == false);
+  ASSERT(match("", "$") == false);
+
+  ASSERT(match("a", "^a") == true);
+  ASSERT(match("ba", "^a") == false);
+  ASSERT(match("^a", "^^a") == true);
+
   ASSERT(match("a", "a$") == true);
-  ASSERT(match("slogs", "log$") == false);
-  ASSERT(match("slogs", "^slogs$") == true);
-  ASSERT(match("log", "^slogs$") == false);
+  ASSERT(match("ba", "b$") == false);
+  ASSERT(match("a", "a$$") == false);
+
+  ASSERT(match("a", "^a$") == true);
+  ASSERT(match("ab", "^a$") == false);
 }
 
 void test_quantifiers() {
-  ASSERT(match("aa", "a+") == true);
-  ASSERT(match("b", "a+") == false);
-  ASSERT(match("b", "a*") == true);
   ASSERT(match("a", "a*") == true);
   ASSERT(match("aa", "a*") == true);
+  ASSERT(match("b", "a*") == true);
+  ASSERT(match("aba", "a*") == true);
   ASSERT(match("bca", "a*") == true);
   ASSERT(match("ba", "a*") == true);
+
+  // TODO 
   ASSERT(match("", "a*") == true);
 }
 
 void test_alternation() {
-  ASSERT(match("catdog", "(cat|dog)+") == true);
-  ASSERT(match("cat", "(bird|dog)") == false);
-  // ASSERT(match("cat", "(|dog)") == false);
-  // ASSERT(match("cat", "(") == false);
-  // ASSERT(match("cat", "(|dog|)") == false);
-  // ASSERT(match("cat", "()") == false);
+  ASSERT(match("dog", "cat|dog") == true);
+  ASSERT(match("bird", "cat|dog|bird") == true);
+  ASSERT(match("cat", "bird|dog") == false);
+  ASSERT(match("cat|", "cat\\|") == true);
+}
+
+void test_capture_group() {
+  // ASSERT(match("dog", "(dog)"));
+  // ASSERT(match("dogdogcat", "(dog)+cat"));
+  ASSERT(match("dogbirdcat", "(dog|bird)+cat") == true);
 }
 
 void run_test_cases() {
-  test_char_only();
-  test_character_class();
-  test_groups();
-  test_anchors();
-  test_quantifiers();
-  test_wildcard();
-  test_alternation();
+  // test_char_only();
+  // test_character_class();
+  // test_groups();
+  // test_anchors();
+  // test_quantifiers();
+  // test_wildcard();
+  // test_alternation();
+  test_capture_group();
 }
 
+
 int main(int argc, char * argv[]) {
+  // return 0;
 
   if (argc != 2) {
     printf("USAGE: string | cgrep PATTERN\n");
@@ -427,18 +382,16 @@ int main(int argc, char * argv[]) {
   }
 
   char buf[BUFSZ];
-  while(fgets(buf, sizeof buf, stdin) != NULL) {
-    int len_line = strlen(buf);
+  fgets(buf, sizeof buf, stdin);
+  int len_line = strlen(buf);
 
-    if (buf[len_line-1] == '\n') {
-      buf[len_line-1] = '\0';
-      if (match(buf, pattern)) {
-        printf("%s\n", buf);
-      }
-    } else {
-      printf("ERROR: line too long got %d max = %d", len_line, BUFSZ);
-      return false;
+  if (buf[len_line-1] == '\n') {
+    if (match(buf, argv[1])) {
+      return true;
     }
+  } else {
+    printf("ERROR: line too long got %d max = %d", len_line, BUFSZ);
+    return false;
   }
 }
   
